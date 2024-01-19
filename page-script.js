f23c21c55b1b6c0d76180cd4e836ea54f5f7127b
roee21 <54744798+roee21@users.noreply.github.com>
2024-01-19 16:03:23 +0200

    Create page-script.js

---
+++ page-script.js
@@ -0,0 +1,69 @@
+console.log("Links connected");
+
+// Change color of the lines when hovering link
+$("a")
+  .on("mouseenter", function () {
+    $("[line]").css("background-color", "var(--light--cursor--line--hover)");
+    $(".cursor-position-wrap").css("opacity", "0");
+    $(".cursor-inner").addClass("is--link");
+
+    // css("background-color", "var(--light--cursor--hover)");
+  })
+  .on("mouseleave", function () {
+    $("[line]").css("background-color", "var(--light--cursor--line--default)");
+    $(".cursor-position-wrap").css("opacity", "1");
+    $(".cursor-inner").removeClass("is--link");
+  });
+
+//Show cursor position
+const box = document.querySelector("body");
+const pageX = document.getElementById("cursor-x");
+const pageY = document.getElementById("cursor-y");
+
+function updateDisplay(event) {
+  pageX.innerText = event.pageX;
+  pageY.innerText = event.pageY;
+}
+
+box.addEventListener("mousemove", updateDisplay, false);
+box.addEventListener("mouseenter", updateDisplay, false);
+box.addEventListener("mouseleave", updateDisplay, false);
+
+//darkmode
+
+function load() {
+  const toggleButton = document.querySelector("#btn-dark");
+  const autoButton = document.querySelector("#btn-auto");
+
+  // MediaQueryList object
+  const useDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+  // Toggles the "dark-mode" class based on if the media query matches
+  function toggleDarkMode(state) {
+    document.documentElement.classList.toggle("dark-mode", state);
+  }
+
+  // Initial setting
+  // toggleDarkMode(useDark.matches);
+
+  // Listen for changes in the OS settings
+  useDark.addListener((evt) => toggleDarkMode(evt.matches));
+
+  // Toggles the "dark-mode" class on click
+  toggleButton.addEventListener("click", () => {
+    let textButton = document.getElementById("color-mode-text");
+    let text = textButton.textContent;
+
+    document.documentElement.classList.toggle("dark-mode");
+
+    if ($("html").hasClass("dark-mode")) {
+      $(textButton).text("light");
+      localStorage.setItem("dark-mode", "true");
+    } else {
+      $(textButton).text("dark");
+      localStorage.removeItem("dark-mode");
+    }
+  });
+}
+
+window.addEventListener("DOMContentLoaded", load);
